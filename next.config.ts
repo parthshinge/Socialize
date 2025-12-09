d11b9654f0f7683318bea32ae568fc3922d82828
parthshinge <shingeparth@gmail.com>
2025-12-09 19:16:51 +0530

    Configure Vercel to trace and include Prisma files and SQLite db

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['prisma', '@prisma/client'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./prisma/**/*'],
+  },
 };
 
 export default nextConfig;
